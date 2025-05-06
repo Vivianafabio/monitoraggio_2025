5dffb2d890b9e9277fe596f1adade57b144ae7e1
Vivianafabio <viviana.fabio23@gmail.com>
2025-05-06 12:43:50 +0200

    Update first_code.R

--- first_code.R
+++ first_code.R
@@ -6,3 +6,13 @@ setwd("C:/R monitoraggio")
 
 mato = rast("Matogrosso_l5_1992219_lrg.jpg")  #assegna il nome "mato" all'immagine
 plot(mato)      # visualizza l'immagine su R
+
+# Bands:
+# band 1 = near infrared
+# band 2 = red
+# band 3 = green
+
+plotRGB(mato, r=2, g=3, b=1)  #tutto quello che è blu è vegetazione, è foresta pluviale primaria
+plotRGB(mato, r=2, g=1, b=3)  #tutto quello che è verde è vegetazione, è foresta pluviale primaria
+plotRGB(mato, r=1, g=2, b=3)  #tutto quello che è rosso è vegetazione, è foresta pluviale primaria
+
